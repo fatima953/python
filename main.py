93392af3b8635b6a0816bd24bc5333181b282678
fatima953 <nabihajamil44@gmail.com>
2024-09-08 11:37:51 +0800

    while loop

---
+++ main.py
@@ -0,0 +1,18 @@
+#while loop
+#variable 
+i = 0
+#condition
+while i < 11:
+    print(i)
+#increment
+    i = i + 2
+
+#never ending loop
+while True:
+    a = input("do you want to continue yes/no:")
+    if a == "yes" :
+     print("you are in the loop")
+    else:
+     print("you are out of the loop")
+     exit()
+
