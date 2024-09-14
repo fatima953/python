505b6ffb73c501d46136fc4094b41a571faaa57b
fatima953 <nabihajamil44@gmail.com>
2024-09-14 20:36:41 +0800

    nested loop

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-#while loop
-#variable 
-i = 0
-#condition
-while i < 11:
-    print(i)
-#increment
-    i = i + 2
-
-#never ending loop
-while True:
-    a = input("do you want to continue yes/no:")
-    if a == "yes" :
-     print("you are in the loop")
-    else:
-     print("you are out of the loop")
-     exit()
-
+print("half pyramad pattern")
+n = int(input("enter the number of rows:"))
+for i in range(n):
+    for j in range(i + 1):
+        print("fatima",end = " ")
+    print()
