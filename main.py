5a84cad84864ce6794816d90979b66223437991a
fatima953 <nabihajamil44@gmail.com>
2024-09-15 09:30:44 +0800

    While loop acp

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-print("half pyramad pattern")
-n = int(input("enter the number of rows:"))
-for i in range(n):
-    for j in range(i + 1):
-        print("fatima",end = " ")
-    print()
+#while loop 
+#variable
+i = 5
+#condition
+while i < 11:
+    print(i)
+
+#increment
+i = i + 2
+
+#never ending loop
+while True:
+   a = input("do you want more pizza?, yes/no:") 
+   if a == "yes":
+       print("here you are, a piece of pizza")
+   else: 
+       print("say  bye bye to pizza")
+       exit()
