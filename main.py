851af6a3e644a6ef9d987a09d44749a366e31fd7
fatima953 <nabihajamil44@gmail.com>
2024-09-22 11:03:48 +0800

    functions acp

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-#while loop 
-#variable
-i = 5
-#condition
-while i < 11:
-    print(i)
-
-#increment
-i = i + 2
-
-#never ending loop
-while True:
-   a = input("do you want more pizza?, yes/no:") 
-   if a == "yes":
-       print("here you are, a piece of pizza")
-   else: 
-       print("say  bye bye to pizza")
-       exit()
+a = int(input("enter a num here: "))
+b = int(input("enter b num here:"))
+print("the answer of a + b is ", a + b)
