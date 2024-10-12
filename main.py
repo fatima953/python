41bf46ddf55311bfb88be940b970c7cf659e5f4d
fatima953 <nabihajamil44@gmail.com>
2024-10-12 18:35:18 +0800

    keywords acp

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-def icecream(a, b = 50):
-    return a * b
-num = input("how many icecreams do you want to eat?, type answer here: ")
-print(icecream(num))
+a = input("enter a word:")
+for i in a:
+    if i == 'a' or 'b' or 'c':
+        print("the letter is found ?(a or b or c)")
+        break
+    else:
+        print("letter not found (a or b or c)")
+#continue "pass"
+for i in range(50):
+    if i % 2 == 0:
+        print(i)
+    elif i % 3 == 0:
+        print(i)
+    elif i % 5 == 0:
+        pass
