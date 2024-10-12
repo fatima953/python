06a0317a0bb422a3f7acf15bc1c292c6b1602fa6
fatima953 <nabihajamil44@gmail.com>
2024-10-12 18:46:06 +0800

    EH acp

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-a = input("enter a word:")
-for i in a:
-    if i == 'a' or 'b' or 'c':
-        print("the letter is found ?(a or b or c)")
-        break
-    else:
-        print("letter not found (a or b or c)")
-#continue "pass"
-for i in range(50):
-    if i % 2 == 0:
-        print(i)
-    elif i % 3 == 0:
-        print(i)
-    elif i % 5 == 0:
-        pass
+try:
+    num = int(input("enter number here:"))
+    print(num)
+except ValueError as ex:
+    print("Exception:" ,ex)
+print("done")
+
+num = int(input("enter number here:"))
+print(num)
+
+try:
+    num1 = int(input("enter number here:"))
+    num2 = int(input("enter number here:"))
+    print(num1/num2)
+except ValueError as ex:
+    print("Exception:" ,ex)
+except ZeroDivisionError as ex:
+    print("Exception:" ,ex)
+except SyntaxError as ex:
+    print("Exception:" ,ex)
+finally:
+    print("done")
