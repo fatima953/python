1fd111a18db72799acd729994010e933ca58f4e9
fatima953 <nabihajamil44@gmail.com>
2024-10-13 11:23:07 +0800

    function acp

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-try:
-    num = int(input("enter number here:"))
-    print(num)
-except ValueError as ex:
-    print("Exception:" ,ex)
-print("done")
-
-num = int(input("enter number here:"))
-print(num)
-
-try:
-    num1 = int(input("enter number here:"))
-    num2 = int(input("enter number here:"))
-    print(num1/num2)
-except ValueError as ex:
-    print("Exception:" ,ex)
-except ZeroDivisionError as ex:
-    print("Exception:" ,ex)
-except SyntaxError as ex:
-    print("Exception:" ,ex)
-finally:
-    print("done")
+def calculator(a,b,c):
+   if c == 1:
+       print("sum is ",a + b)
+   elif c == 2:
+       print("subtract is ",a - b)
+   elif c == 3:
+       print("multiplication is ",a * b)
+   elif c == 4:
+       print("division is ",a / b)
+   else:
+       print("invalid option")
+print("calculator")
+print("1.add \n 2.subtract \n 3.multiply \n 4.divide")
+choise = int(input("enter your choise:"))
+num1 = float(input("enter number here:"))
+num2 = float(input("enter number here:"))
+calculator(num1,num2,choise)
