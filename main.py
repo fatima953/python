9ff5e2c59b973692b45e14d2c290ebafaeb2eaf2
fatima953 <nabihajamil44@gmail.com>
2024-10-27 10:36:10 +0800

    dictionary

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import calendar
-print(calendar.month(2024,1))
-print(calendar.month(2024,8))
-print(calendar.month(2024,10))
-print(calendar.month(2024,11))
+#dictionary
+student = {"name":"Fatima", "age": 10, "grade": 5}
+print(student)
+print("my name is", student["name"])
+print("i am ",student["age"],"years old")
+print("i am in grade",student["grade"])
+student["country"] = "China"
+student["hobby"] = "skating"
+print(student)
+del student["country"]
+print(student)
+print(len(student))
+student.clear()
+print(student)
+#country code
+country_code = {"China" : 86, "India" : 91, "Pakistan" : 92}
+print(country_code.get("Pakistan", "not found"))
+print(country_code.get("Nepal", "not found"))
