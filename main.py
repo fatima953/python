ed7b7e12d0653a2648a5216cb1b9380a6e45cdc9
fatima953 <nabihajamil44@gmail.com>
2024-10-27 11:08:23 +0800

    number swap acp

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-import math
-a = float(input("enter a decimal number:"))
-print("ceil:", math.ceil(a))
-print("floor:", math.floor(a))
-print("Welcome to guess the number game")
-print("i will guess a number, you need to guess my number between 1 and 100")
-import random 
-ran_choise =  random.randint(1,100)
-while True:
-    try:
-     num = int(input("enter the number"))
-     if num == ran_choise:
-        print("yes, you got the number")
-        break
-     elif num < ran_choise:
-        print("the number is bigger then your guess")
-     elif num > ran_choise:
-        print("the number is smaller then your then your guess")
-    except ValueError as er:
-        print("invalid input")
-    
-print("thank you for playing")
+a = 3
+b = 2
+c = 1 
+bucket = b
+temp = a
+b = a 
+a = c
+c = bucket
+print("after swaping", a,b,c)
