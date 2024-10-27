0aa60073289aec4b8db9271e9d66af32fc0f6bfe
fatima953 <nabihajamil44@gmail.com>
2024-10-27 10:54:51 +0800

    keywords acp

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-
-student = ["Fatima", 10, 5, "China"]
-print(student)
-student.append("My hobby is skating")
-print(student)
-print("numbers of items in the lists", len(student))
-a = [3,4,4,5,3,3,4,9,8]
-print("maximum number in the list", max(a))
-print("minimum number in the list", min(a))
-(a.sort())
-print(a)
-a.reverse()
-print(a)
-print("first value in the list", a[0])
-print("third value in the list", a[3])
-print(a[6:9])
-a.insert(9,5)
-print(a)
-print(a.index(8))
-a.remove(3)
-print(a)
+a = input("enter a word:")
+for i in a:
+    if i == 'a' or 'b' or 'c':
+        print("the letter is found ?(a or b or c)")
+        break
+    else:
+        print("letter not found (a or b or c)")
+#continue "pass"
+for i in range(50):
+    if i % 2 == 0:
+        print(i)
+    elif i % 3 == 0:
+        print(i)
+    elif i % 5 == 0:
+        pass
