06e6d69067fffb910e3a5197f9be3ac18246ced2
fatima953 <nabihajamil44@gmail.com>
2024-10-27 10:32:00 +0800

    DTACM acp

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-def calculator(a,b,c):
-   if c == 1:
-       print("sum is ",a + b)
-   elif c == 2:
-       print("subtract is ",a - b)
-   elif c == 3:
-       print("multiplication is ",a * b)
-   elif c == 4:
-       print("division is ",a / b)
-   else:
-       print("invalid option")
-print("calculator")
-print("1.add \n 2.subtract \n 3.multiply \n 4.divide")
-choise = int(input("enter your choise:"))
-num1 = float(input("enter number here:"))
-num2 = float(input("enter number here:"))
-calculator(num1,num2,choise)
+import calendar
+print(calendar.month(2024,1))
+print(calendar.month(2024,8))
+print(calendar.month(2024,10))
+print(calendar.month(2024,11))
